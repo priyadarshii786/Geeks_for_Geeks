65e232ca8333cfb4f0b7a9b9e1f342e182bee843
priyadarshii786 <tathagatp918@gmail.com>
2023-05-15 15:16:16 +0530

    3.14pm

---
+++ 8. Sum of n numbers.cpp
@@ -0,0 +1,13 @@
+//   https://practice.geeksforgeeks.org/batch/cpp-stl-1/track/foundation-cpp-operators/problem/sum-of-n-number
+
+int nSum(int n)
+{
+    int ans = 0;
+
+    // Write your code here to calculate and return sum of n number.
+    //  jai ganesh
+
+    ans = (n * (n + 1)) / 2;
+
+    return ans;
+}
