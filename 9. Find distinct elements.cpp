ff33229309316b52f39143b1a1a0c96f5c2b840c
priyadarshii786 <tathagatp918@gmail.com>
2023-06-04 23:55:24 +0530

    used concept of mapping

---
+++ 9. Find distinct elements.cpp
@@ -0,0 +1,27 @@
+//  https://practice.geeksforgeeks.org/batch/cpp-stl-1/track/foundation-cpp-array/problem/find-distinct-elements2054
+
+class Solution
+{
+public:
+    int distinct(vector<vector<int>> M, int N)
+    {
+        // code here
+        // jai ganesh
+        map<int, int> num;
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (num[M[i][j]] == i)
+                    num[M[i][j]]++;
+            }
+        }
+        int distinct_count = 0;
+        for (auto i : num)
+        {
+            if (i.second == N)
+                distinct_count++;
+        }
+        return distinct_count;
+    }
+};
