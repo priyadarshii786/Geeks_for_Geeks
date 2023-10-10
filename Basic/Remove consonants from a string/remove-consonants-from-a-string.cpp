bf3572a61fd293d60305983a2cdd69ba07985e13
Tathagat Priyadarshi <100780858+priyadarshii786@users.noreply.github.com>
2023-10-10 15:57:00 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Remove consonants from a string/remove-consonants-from-a-string.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    string removeConsonants(string s){
+    //complete the function heredef removeConsonants(s):
+    // jai ganesh
+    
+    string r;
+    for ( const char c : s )
+        switch ( c ) {
+            case 'a': case 'e': case 'i': case 'o': case 'u':
+            case 'A': case 'E': case 'I': case 'O': case 'U':
+                r += c;
+        }
+    return r.empty() ? "No Vowel" : r;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.removeConsonants(s) << "\n";
+    }
+return 0;
+}
+// } Driver Code Ends
