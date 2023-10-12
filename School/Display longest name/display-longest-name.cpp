c49dc37ebc3ae83fb9c26d1a6f84b92a3bd2cb86
Tathagat Priyadarshi <100780858+priyadarshii786@users.noreply.github.com>
2023-10-13 00:42:29 +0530

    Added Solution - GfG to GitHub

---
+++ School/Display longest name/display-longest-name.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    string longest(string names[], int n)
+    {
+        // jai ganesh
+        int l=0;
+        for(int i=0;i<n;i++){
+            if(names[l].length()<names[i].length()){
+                l=i;
+            }
+        }
+        return names[l];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		string names[n];
+		
+		for(int i=0;i<n;i++)
+			cin>>names[i];
+		Solution ob;
+		cout<< ob.longest(names, n) << endl;
+	}
+}
+
+// } Driver Code Ends
