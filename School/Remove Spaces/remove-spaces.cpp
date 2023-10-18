448104c0f7d72821a1b53e8f33836750340aa5b0
Tathagat Priyadarshi <100780858+priyadarshii786@users.noreply.github.com>
2023-10-18 21:28:22 +0530

    Added Solution - GfG to GitHub

---
+++ School/Remove Spaces/remove-spaces.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    string modify (string s)
+    {
+        //code here.
+        return replace( s.begin(), s.end(), ' ', {} ), s;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore ();
+    while (t--)
+    {
+        string s;
+        getline (cin, s);
+        Solution ob;
+        cout <<ob.modify (s) << endl;
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
