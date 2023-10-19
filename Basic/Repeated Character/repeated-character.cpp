e50f470dfb3142a177d439763c201ce771edb6c1
Tathagat Priyadarshi <100780858+priyadarshii786@users.noreply.github.com>
2023-10-20 00:32:39 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Repeated Character/repeated-character.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    char firstRep (string s)
+    {
+        //code here.
+        //jai ganesh
+        for(int i=0;i<s.size();i++){
+            if(count(s.begin(),s.end(),s[i]) > 1){
+                return s[i];
+            }
+        }
+        return '#';
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        Solution ob;
+        char res = ob.firstRep (s);
+        if (res == '#') cout << -1 << endl;
+        else cout << res << endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
