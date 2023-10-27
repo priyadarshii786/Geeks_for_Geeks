1ad3681d1590892c9f60a19e58f2f3857638ce9e
Tathagat Priyadarshi <100780858+priyadarshii786@users.noreply.github.com>
2023-10-27 17:37:07 +0530

    Added Solution - GfG to GitHub

---
+++ School/Reversing the vowels/reversing-the-vowels.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        string modify (string s)
+        {
+            //code here.
+            //jai ganesh
+            stack<int> st;
+            string temp="";
+            for(int i=0;i<s.length();i++)
+            {
+                if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
+                {
+                    st.push(s[i]);
+                }
+            }
+            for(int i=0;i<s.length();i++)
+            {
+                if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
+                {
+                    s[i]=st.top();
+                    st.pop();
+                }
+            }
+            return s;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        Solution ob;
+        cout <<ob.modify (s) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
