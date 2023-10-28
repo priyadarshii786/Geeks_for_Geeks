309a6bfca4e846898a4ca8b8277fb895977a5c17
Tathagat Priyadarshi <100780858+priyadarshii786@users.noreply.github.com>
2023-10-28 20:59:11 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Binary String/binary-string.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to count the number of substrings that start and end with 1.
+    long binarySubstring(int n, string a){
+        
+        // Your code here
+        // jai ganesh
+        const long c{ count( a.cbegin(), a.cend(), '1' ) };
+        return c * ( c - 1 ) / 2;
+        
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n,count;
+    string a;
+    cin>>t;
+    while(t--)
+    {
+        count = 0;
+        cin >> n;
+        cin >> a;
+        Solution obj;
+        cout << obj.binarySubstring(n, a) << endl;
+    }
+}
+
+// } Driver Code Ends
